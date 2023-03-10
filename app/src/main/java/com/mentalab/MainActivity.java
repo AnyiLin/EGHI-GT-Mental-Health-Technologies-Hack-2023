65dad7b72bdae14b966adc11f51e62f3518d3e1f
brotherhobo <anyo3223@gmail.com>
2023-03-10 13:32:28 -0500

    more work on bluetooth connect page

--- app/src/main/java/com/mentalab/MainActivity.java
+++ app/src/main/java/com/mentalab/MainActivity.java
@@ -36,12 +36,13 @@ public class MainActivity extends AppCompatActivity {
   private AppBarConfiguration appBarConfiguration;
   private ActivityMainBinding binding;
 
+  public boolean impMode = false;
+  public int device = 1;
+
   @RequiresApi(api = VERSION_CODES.R)
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    int device = 1;
-    boolean impMode = false;
 
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
@@ -49,61 +50,15 @@ public class MainActivity extends AppCompatActivity {
     NavHostFragment navHostFragment =
             (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
     NavController navController = navHostFragment.getNavController();
-
-    try {
-      ExploreDevice connect = null;
-      if(device == 0) connect = MentalabCommands.connect("8524");
-      else if(device == 1) connect = MentalabCommands.connect("CA14");
-      else if(device == 2) connect = MentalabCommands.connect("844C");
-      else throw new IllegalArgumentException("Device ID unknown");
-      connect.acquire();
-      Subscriber<EEGPacket> sub = null;
-      Subscriber<MarkerPacket> markerSub = new Subscriber<MarkerPacket>(Topic.MARKER) {
-        @Override
-        public void accept(Packet packet) {
-          Log.d("Got marker", packet.getData().toString());
-        }
-      };
-
-      if(impMode) {
-        connect.calculateImpedance(); // something here is wrong for 32 channels
-        sub = new Subscriber<EEGPacket>(Topic.IMPEDANCE) {
-          @Override
-          public void accept(Packet packet) {
-            Log.d("DEBUG__ZZ", packet.getData().toString());
-          }
-        };
-      }
-      else {
-        sub = new Subscriber<EEGPacket>(Topic.EXG) {
-          @Override
-          public void accept(Packet packet) {
-            Log.d("DEBUG__ZZ", packet.getData().toString());
-          }
-        };
-      }
-      ContentServer.getInstance().registerSubscriber(sub);
-      ContentServer.getInstance().registerSubscriber(markerSub);
-
-      // To get last connected device after sending any command/connection drop: use
-      // getLastConnectedDevice() method of MentalabCodec
-    }
-    // catch (NoBluetoothException | NoConnectionException | IOException | ExecutionException |
-    // InterruptedException e) {
-    catch (NoBluetoothException
-        | NoConnectionException
-        | IOException
-        | ExecutionException
-        | InterruptedException
-        | InvalidCommandException
-        | CommandFailedException e) {
-      e.printStackTrace();
-    };
   }
 
   // Can be used to test if UI Thread is blocked while running demo
   public void sayHello(View view) {
     Toast toast = Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT);
     toast.show();
+  }
+
+  public boolean getImpMode() {
+    return impMode;
   }
 }
--- app/src/main/java/com/mentalab/ui/main/BluetoothFragment.java
+++ app/src/main/java/com/mentalab/ui/main/BluetoothFragment.java
@@ -1,6 +1,7 @@
 package com.mentalab.ui.main;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,10 +18,25 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.transition.Visibility;
 
+import com.mentalab.ExploreDevice;
+import com.mentalab.MainActivity;
+import com.mentalab.MentalabCommands;
 import com.mentalab.R;
 import com.mentalab.databinding.BluetoothBinding;
+import com.mentalab.exception.CommandFailedException;
+import com.mentalab.exception.InvalidCommandException;
+import com.mentalab.exception.NoBluetoothException;
+import com.mentalab.exception.NoConnectionException;
+import com.mentalab.packets.Packet;
+import com.mentalab.packets.sensors.MarkerPacket;
+import com.mentalab.packets.sensors.exg.EEGPacket;
+import com.mentalab.service.io.ContentServer;
+import com.mentalab.service.io.Subscriber;
+import com.mentalab.utils.constants.Topic;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class BluetoothFragment extends Fragment {
     public MainViewModel mainViewModel;
@@ -57,6 +73,60 @@ public class BluetoothFragment extends Fragment {
         });
 
         bluetoothScrollIDs = addBluetoothConnectScrollView();
+    }
+
+    public void connectToEEG() {
+        boolean impMode = ((MainActivity)getActivity()).impMode;
+        int device = ((MainActivity)getActivity()).device;
+        try {
+            ExploreDevice connect = null;
+            if(device == 0) connect = MentalabCommands.connect("8524");
+            else if(device == 1) connect = MentalabCommands.connect("CA14");
+            else if(device == 2) connect = MentalabCommands.connect("844C");
+            else throw new IllegalArgumentException("Device ID unknown");
+            connect.acquire();
+            Subscriber<EEGPacket> sub = null;
+            Subscriber<MarkerPacket> markerSub = new Subscriber<MarkerPacket>(Topic.MARKER) {
+                @Override
+                public void accept(Packet packet) {
+                    Log.d("Got marker", packet.getData().toString());
+                }
+            };
+
+            if(impMode) {
+                connect.calculateImpedance(); // something here is wrong for 32 channels
+                sub = new Subscriber<EEGPacket>(Topic.IMPEDANCE) {
+                    @Override
+                    public void accept(Packet packet) {
+                        Log.d("DEBUG__ZZ", packet.getData().toString());
+                    }
+                };
+            }
+            else {
+                sub = new Subscriber<EEGPacket>(Topic.EXG) {
+                    @Override
+                    public void accept(Packet packet) {
+                        Log.d("DEBUG__ZZ", packet.getData().toString());
+                    }
+                };
+            }
+            ContentServer.getInstance().registerSubscriber(sub);
+            ContentServer.getInstance().registerSubscriber(markerSub);
+
+            // To get last connected device after sending any command/connection drop: use
+            // getLastConnectedDevice() method of MentalabCodec
+        }
+        // catch (NoBluetoothException | NoConnectionException | IOException | ExecutionException |
+        // InterruptedException e) {
+        catch (NoBluetoothException
+                | NoConnectionException
+                | IOException
+                | ExecutionException
+                | InterruptedException
+                | InvalidCommandException
+                | CommandFailedException e) {
+            e.printStackTrace();
+        };
     }
 
     public ArrayList<Integer> addBluetoothConnectScrollView() {
