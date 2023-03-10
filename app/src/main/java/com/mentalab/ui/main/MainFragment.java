8f3a549b7f26ca7544720e611b2a2093b8566aad
brotherhobo <anyo3223@gmail.com>
2023-03-09 20:34:52 -0500

    small gradle and bluetooth connection list changes

--- app/src/main/java/com/mentalab/ui/main/MainFragment.java
+++ app/src/main/java/com/mentalab/ui/main/MainFragment.java
@@ -1,5 +1,6 @@
 package com.mentalab.ui.main;
 
+import androidx.annotation.IdRes;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -12,12 +13,21 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.mentalab.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MainFragment extends Fragment {
 
     public MainViewModel mainViewModel;
+
+    public ArrayList<Integer> bluetoothScrollIDs;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -41,6 +51,31 @@ public class MainFragment extends Fragment {
                         .navigate(R.id.action_mainFragment_to_bluetoothFragment);
             }
         });
+
+        bluetoothScrollIDs = addBluetoothConnectScrollView();
+    }
+
+    public ArrayList<Integer> addBluetoothConnectScrollView() {
+        ScrollView bluetoothScroll = (ScrollView) getView().findViewById(R.id.bluetoothScrollView);
+        bluetoothScroll.setVisibility(View.VISIBLE);
+        ArrayList<Integer> linearLayoutIDList = new ArrayList<>();
+        for (int counter = 0; counter < 3; counter++) {
+            LinearLayout linearLayout = new LinearLayout(getContext());
+            Button connectButton = new Button(getContext());
+            TextView connectText = new TextView(getContext());
+            linearLayout.addView(connectButton);
+            linearLayout.addView(connectText);
+            connectButton.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+            connectButton.setText("Connect");
+            connectText.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+            connectText.setText("device "+counter);
+            bluetoothScroll.addView(linearLayout);
+            linearLayout.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+            int newId = View.generateViewId();
+            linearLayout.setId(newId);
+            linearLayoutIDList.add(newId);
+        }
+        return linearLayoutIDList;
     }
 
 }
